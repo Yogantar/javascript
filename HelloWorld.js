def99af586779b6fb79987f33fda124c04df5c96
Yogantar <Yogantar.Chhibber@coforge.com>
2021-12-03 01:31:01 +0530

    Final Commit 1.0

---
+++ HelloWorld.js
@@ -0,0 +1,35 @@
+document.writeln("Hello World from Java Script");
+
+document.writeln("<h1>Hello World from Java Script");
+
+/* Java Script code to add 2 nos */
+
+var a,b,sum;
+
+
+
+var Name=prompt("Enter Your Name :");
+
+ a=parseInt(prompt("Enter first Number:","Enter a Number"));
+
+ b=parseInt(prompt("Enter second Number:","Enter a Number"));
+
+
+
+sum=a+b;
+
+document.write("<br>"+"Hello "+Name+"<br>");
+
+document.writeln("The Addition of 2 nos is :"+sum);
+
+ 
+
+alert("Thank You "+Name);
+
+
+
+// Ctrl + / = Single line comment
+
+
+
+// Select multiple lines -  Alt+ Shift + a = Multi line comments
